eb4416d0cbb1f147eeb6a4552261eeb69ece14a2
DucHo <hohuuduc2709@gmail.com>
2024-06-07 01:31:09 +0700

    Draft

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 import { program } from 'commander';
+import intro from './intro';
+import log from './log';
+import fs from 'fs';
+import pkg, { GitHubTreeUrl } from './env';
 
-const log = (...data: string[]): void => { console.log(...data); };
-
-/*log(` ____                                    ___       ______                               ___              __              
-/\\  _\`\\                                 /\\_ \\     /\\__  _\\                             /\\_ \\            /\\ \\__           
-\\ \\ \\L\\ \\   __      _ __    ___      __ \\//\\ \\    \\/_/\\ \\/    ___     ___ ___    _____ \\//\\ \\     ____  \\ \\ ,_\\     __   
- \\ \\ ,__/ /'__\`\\   /\\\`'__\\ /'___\\  /'__\`\\ \\ \\ \\      \\ \\ \\  / '__\`\\ /' __\` __\`\\ /\\ '__\`\\ \\ \\ \\  / /'__\`\\ \\ \\ \\/   /'__\`\\ 
-  \\ \\ \\/ /\\ \\L\\.\\_ \\ \\ \\/ /\\ \\__/ /\\  __/  \\_\\ \\_     \\ \\ \\ /\\  __/ /\\ \\/\\ \\/\\ \\\\ \\ \\L\\ \\ \\_\\ \\_\\ \\ \\L\\.\\_\\ \\ \\_ /\\  __/ 
-   \\ \\_\\ \\ \\__/.\\_\\ \\ \\_\\ \\ \\____\\\\ \\____\\ /\\____\\     \\ \\_\\\\ \\____\\\\ \\_\\ \\_\\ \\_\\\\ \\ ,__/ /\\____\\\\ \\__/.\\_\\\\ \\__\\\\ \\____\\
-    \\/_/  \\/__/\\/_/  \\/_/  \\/____/ \\/____/ \\/____/      \\/_/ \\/____/ \\/_/\\/_/\\/_/ \\ \\ \\   \\/____/ \\/__/\\/_/ \\/__/ \\/____/
-                                                                                   \\ \\_\\                                      
-                                                                                    \\/_/                                      `)*/
-
+//intro()
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .name(pkg.displayName)
+  .description(pkg.description)
+  .version(pkg.version)
+  .option('-ts, --typescript', 'Apply TypeScript pattern. (default is Javascript)', false)
+  .option('-e, --eslint', 'Apply ESLint configuration. (default no)', false)
+  .option('-f, --full-source', 'In Developing', false);
 
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+program.parse()
 
-program.parse();
+const opts = program.opts()
+
+async () => {
+    const result = await fetch(GitHubTreeUrl).then(res => res.text())
+    console.log(result)
+  }
+
+
+// try {
+//   fs.cpSync(__dirname + "/template", process.cwd(), { recursive: true })
+// } catch (error) {
+//   log("Unable to initialize resource")
+// }
---
+++ src/log.ts
@@ -0,0 +1,3 @@
+export default function log(...data: any[]): void {
+    console.log(...data);
+}

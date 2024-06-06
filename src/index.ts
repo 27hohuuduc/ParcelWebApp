e6c1c3ea5c070e77dbee083b65906091ddc5f545
27hohuuduc <27hohuuduc@gmail.com>
2024-06-06 15:00:55 +0900

    Example string-util.js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { program } from 'commander';
 
-const log = (...data: string[]): void => { console.log(data); };
+const log = (...data: string[]): void => { console.log(...data); };
 
-log(` ____                                    ___       ______                               ___              __              
+/*log(` ____                                    ___       ______                               ___              __              
 /\\  _\`\\                                 /\\_ \\     /\\__  _\\                             /\\_ \\            /\\ \\__           
 \\ \\ \\L\\ \\   __      _ __    ___      __ \\//\\ \\    \\/_/\\ \\/    ___     ___ ___    _____ \\//\\ \\     ____  \\ \\ ,_\\     __   
  \\ \\ ,__/ /'__\`\\   /\\\`'__\\ /'___\\  /'__\`\\ \\ \\ \\      \\ \\ \\  / '__\`\\ /' __\` __\`\\ /\\ '__\`\\ \\ \\ \\  / /'__\`\\ \\ \\ \\/   /'__\`\\ 
@@ -10,15 +10,22 @@ log(` ____                                    ___       ______                  
    \\ \\_\\ \\ \\__/.\\_\\ \\ \\_\\ \\ \\____\\\\ \\____\\ /\\____\\     \\ \\_\\\\ \\____\\\\ \\_\\ \\_\\ \\_\\\\ \\ ,__/ /\\____\\\\ \\__/.\\_\\\\ \\__\\\\ \\____\\
     \\/_/  \\/__/\\/_/  \\/_/  \\/____/ \\/____/ \\/____/      \\/_/ \\/____/ \\/_/\\/_/\\/_/ \\ \\ \\   \\/____/ \\/__/\\/_/ \\/__/ \\/____/
                                                                                    \\ \\_\\                                      
-                                                                                    \\/_/                                      `)
+                                                                                    \\/_/                                      `)*/
+
 
 program
-  .option('--first')
-  .option('-s, --separator <char>');
+  .name('string-util')
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
+
+program.command('split')
+  .description('Split a string into substrings and display as an array')
+  .argument('<string>', 'string to split')
+  .option('--first', 'display just the first substring')
+  .option('-s, --separator <char>', 'separator character', ',')
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
 
 program.parse();
-
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-const string = program.args[0].split(options.separator, limit)
-log(...string);
